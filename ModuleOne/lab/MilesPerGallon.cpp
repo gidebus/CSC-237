d2a3cb485943024361d559b899f1dd386f2ff7a1
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-09-12 22:34:06 -0400

    Add input validation

--- ModuleOne/lab/MilesPerGallon.cpp
+++ ModuleOne/lab/MilesPerGallon.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -12,24 +13,66 @@ float calculateMilesPerGallon(float milesTraveled, float gallonsPurchased) {
   return milesPerGallon;
 }
 
+int validateInitialOdometerInput(int initialOdometer) {
+  while(true) {
+    if (
+      (cin >> initialOdometer) && 
+      (initialOdometer == static_cast<int>(initialOdometer)) && 
+      (initialOdometer >= 0)
+    ) {
+      cin.ignore(100,'\n');
+      return initialOdometer;
+    }
+    cin.clear();
+    cin.ignore(100,'\n');
+    cout << "Please enter a positive integer: ";
+  }
+}
+
+int validateLatestOdometerInput(int initialOdometer, int finalOdometer) {
+  while(true) {
+    if (cin >> finalOdometer && finalOdometer > initialOdometer) {
+      cin.ignore(100, '\n');
+      return finalOdometer;
+    }
+    cin.clear();
+    cin.ignore(100,'\n');
+    cout << "Please enter a positive integer greater than previous odometer: ";
+  }
+}
+
+float validateGallonsPurchased(float gallonsPurchased) {
+  while(true) {
+    if(cin >> gallonsPurchased && gallonsPurchased > 0) {
+      return gallonsPurchased;
+    }
+    cin.clear();
+    cin.ignore(100, '\n');
+    cout << "Please enter a positive decimal number greater than 0: ";
+  }
+}
+
 int main() {
   int odometerAtPreviousFillUp, odometerAtLatestFillUp;
   float milesTraveled, gallonsPurchasedAtLatestFillUp, milesPerGallon;
 
   cout << "Enter Odometer at PREVIOUS fill-up: ";
-  cin >> odometerAtPreviousFillUp;
+  odometerAtPreviousFillUp = validateInitialOdometerInput(odometerAtPreviousFillUp);
+
 
   cout << "Enter Odometer at LATEST fill-up: ";
-  cin >> odometerAtLatestFillUp;
+  odometerAtLatestFillUp = validateLatestOdometerInput(odometerAtPreviousFillUp, odometerAtLatestFillUp);
+
 
   milesTraveled = calculateMilesTraveled(odometerAtLatestFillUp, odometerAtPreviousFillUp);
-  cout << "Distanced traveled is was " << milesTraveled << endl;
+  cout << "Distanced traveled was " << fixed << setprecision(1) << milesTraveled << " miles." << endl;
 
   cout << "Enter gallons purchased at LATEST fill-up: ";
-  cin >> gallonsPurchasedAtLatestFillUp;
+  gallonsPurchasedAtLatestFillUp = validateGallonsPurchased(gallonsPurchasedAtLatestFillUp);
 
-  milesPerGallon = calculateMilesPerGallon(milesTraveled ,gallonsPurchasedAtLatestFillUp);
-  cout << "Your fuel economy is: " << milesPerGallon << endl;
+
+  milesPerGallon = calculateMilesPerGallon(milesTraveled, gallonsPurchasedAtLatestFillUp);
+  cout << "Your fuel economy is: " << fixed << setprecision(1) << milesPerGallon << " MPG." << endl;
 
   return 0;
 }
