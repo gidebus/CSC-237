20e85229666775926f586324b1b7daf1245a2de0
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-11-15 17:11:22 -0500

    Implement non-file io commands

--- project2/ProjectTwo.cpp
+++ project2/ProjectTwo.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <iomanip>
+#include "InventoryItem.h"
 
 using namespace std;
 
@@ -17,15 +18,29 @@ void printMenu() {
   cout << endl;
 }
 
+// TODO: Pass array by reference
+// TODO: Implement try/catch
+// TODO: Break into validating functions? Ex. is not negative, etc.
+
+void addParts(InventoryItem inventory[]);
+void addItem(int &itemCount, InventoryItem inventory[]);
+void printInventory(int &itemCount, InventoryItem inventory[]);
+void removeParts(InventoryItem inventory[]);
+void inputFromFile(int &itemCount, InventoryItem inventory[]);
+int splitLineToArray(string inputText, string inputDelim, string outputFieldArray[], int maxFields);
+
 int main() {
   char userInput;
+  int MAX_ITEMS = 100;
+  InventoryItem inventory[MAX_ITEMS];
+  int itemCount = 0;
 
   do {
     cout << "\nEnter command (or 'h' for help): ";
     cin >> userInput;
 
     if(userInput == 'a') {
-
+      addParts(inventory);
     }
 
     if(userInput == 'h') {
@@ -34,11 +49,11 @@ int main() {
 
 
     if(userInput == 'i') {
-
+      inputFromFile(itemCount, inventory);
     }
 
     if(userInput == 'n') {
-
+      addItem(itemCount, inventory);
     }
 
     if(userInput == 'o') {
@@ -46,19 +61,111 @@ int main() {
     }
 
     if(userInput == 'p') {
+      if(itemCount == 0) {
+        cout << "There are no items yet. " << endl;
+      }
+      printInventory(itemCount, inventory);
+    }
 
+    if(userInput == 'r') {
+      removeParts(inventory);
     }
 
     if(userInput == 'q') {
       break;
     }
 
-    if(userInput == 'r') {
-
-    }
-
   } while(true);
 
   cout << "\nExiting program...";
   return 0;
+}
+
+void addParts(InventoryItem inventory[]) {
+  int itemIndex;
+  int additionalParts;
+  InventoryItem item;
+
+  cout << "Choose a item Number: ";
+  cin >> itemIndex;
+
+  item = inventory[itemIndex];
+
+  if(item.getDescription().empty()) {
+    cout << "Error: item is not in use. Select another item." << endl;
+    return;
+  }
+
+  cout << "How many parts to add? ";
+  cin >> additionalParts;
+  if (additionalParts < 0 || additionalParts + item.getUnits() > 30) {
+    cout << "Error: invalid number of units" << endl;
+    return;
+  }
+
+  item.setUnits(additionalParts + item.getUnits());
+
+}
+
+void addItem(int &itemCount, InventoryItem inventory[]) {
+  InventoryItem newItem;
+  string newItemDescription;
+  double newItemCost;
+  int newItemUnits;
+
+  cout << "Enter description for new Item: ";
+  cin.ignore();
+  getline(cin, newItemDescription);
+
+  cout << "Enter unit cost for new Item: ";
+  cin >> newItemCost;
+
+  cout << "Enter initial quantity for the new item: ";
+  cin >> newItemUnits;
+
+  inventory[itemCount] = InventoryItem(newItemDescription, newItemCost, newItemUnits);
+  itemCount++; // TODO: Account for 0 index initial value
+  cout << "Announcing a new inventory Item: " << newItemDescription << endl;
+  cout << "We now have " << itemCount << " different inventory item(s) in stock!" << endl;
+}
+
+void printInventory(int &itemCount, InventoryItem inventory[]) {
+  // InventoryItem item;
+  cout << "\nItem Num" << setw(15) << "Description" << setw(26) << "Cost" << setw(12) << "Quantity" << endl;
+  cout << "--------" << setw(15) << "-----------" << setw(26) << "----" << setw(12) << "--------" << endl;
+  for(int i = 0; i < itemCount; i++) {
+    // item = inventory[i];
+    cout << i << setw(18) << inventory[i].getDescription() << setw(31) << inventory[i].getCost() << setw(12) << inventory[i].getUnits() << endl;
+  }
+  cout << "\n\n" << itemCount << " records." << endl;
+}
+
+void removeParts(InventoryItem inventory[]) {
+  int itemIndex;
+  int unitsToRemove;
+  InventoryItem item;
+
+  cout << "Choose a item Number: ";
+  cin >> itemIndex;
+
+  item = inventory[itemIndex];
+
+  if(item.getDescription().empty()) {
+    cout << "Error: item is not in use. Select another item." << endl;
+    return;
+  }
+
+  cout << "How many parts to remove? ";
+  cin >> unitsToRemove;
+  if (unitsToRemove < 0 || unitsToRemove > item.getUnits()) {
+    cout << "Error: Invalid number of units" << endl;
+    return;
+  }
+
+  item.setUnits(item.getUnits() - unitsToRemove);
+
+}
+
+void inputFromFile(int &itemCount, InventoryItem inventory[]) {
+
 }
