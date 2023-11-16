1f243e1a73c03a09c6bda32fd54628ea526f22ed
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-11-15 19:22:28 -0500

    Implement I/O to file

--- project2/ProjectTwo.cpp
+++ project2/ProjectTwo.cpp
@@ -1,6 +1,8 @@
 
 #include <iostream>
 #include <iomanip>
+#include <fstream>
+#include <sstream>
 #include "InventoryItem.h"
 
 using namespace std;
@@ -27,6 +29,7 @@ void addItem(int &itemCount, InventoryItem inventory[]);
 void printInventory(int &itemCount, InventoryItem inventory[]);
 void removeParts(InventoryItem inventory[]);
 void inputFromFile(int &itemCount, InventoryItem inventory[]);
+void outputFromFile(int &itemCount, InventoryItem inventory[]);
 int splitLineToArray(string inputText, string inputDelim, string outputFieldArray[], int maxFields);
 
 int main() {
@@ -57,7 +60,7 @@ int main() {
     }
 
     if(userInput == 'o') {
-
+      outputFromFile(itemCount, inventory);
     }
 
     if(userInput == 'p') {
@@ -84,24 +87,27 @@ int main() {
 void addParts(InventoryItem inventory[]) {
   int itemIndex;
   int additionalParts;
-  InventoryItem item;
 
   cout << "Choose a item Number: ";
   cin >> itemIndex;
 
-  item = inventory[itemIndex];
+  InventoryItem &item = inventory[itemIndex];
 
   if(item.getDescription().empty()) {
     cout << "Error: item is not in use. Select another item." << endl;
     return;
   }
 
-  cout << "How many parts to add? ";
-  cin >> additionalParts;
-  if (additionalParts < 0 || additionalParts + item.getUnits() > 30) {
-    cout << "Error: invalid number of units" << endl;
-    return;
-  }
+  do {
+    cout << "How many parts to add? ";
+    cin >> additionalParts;
+
+    if (additionalParts < 0 || additionalParts + item.getUnits() > 30) {
+      cout << "Error: invalid number of units" << endl;
+    } else {
+      break;
+    }
+  } while(true);
 
   item.setUnits(additionalParts + item.getUnits());
 
@@ -131,11 +137,10 @@ void addItem(int &itemCount, InventoryItem inventory[]) {
 
 void printInventory(int &itemCount, InventoryItem inventory[]) {
   // InventoryItem item;
-  cout << "\nItem Num" << setw(15) << "Description" << setw(26) << "Cost" << setw(12) << "Quantity" << endl;
-  cout << "--------" << setw(15) << "-----------" << setw(26) << "----" << setw(12) << "--------" << endl;
+  cout << setw(10) << "\nItem Num" << setw(30) << "Description" << setw(10) << "Cost" << setw(10) << "Quantity" << endl;
+  cout << setw(10) << "--------" << setw(30) << "-----------" << setw(10) << "----" << setw(10) << "--------" << endl;
   for(int i = 0; i < itemCount; i++) {
-    // item = inventory[i];
-    cout << i << setw(18) << inventory[i].getDescription() << setw(31) << inventory[i].getCost() << setw(12) << inventory[i].getUnits() << endl;
+    cout << left << setw(10) << i << setw(30) << inventory[i].getDescription() << setw(10) << fixed << setprecision(2) << inventory[i].getCost() << setw(10) << inventory[i].getUnits() << endl;
   }
   cout << "\n\n" << itemCount << " records." << endl;
 }
@@ -143,12 +148,11 @@ void printInventory(int &itemCount, InventoryItem inventory[]) {
 void removeParts(InventoryItem inventory[]) {
   int itemIndex;
   int unitsToRemove;
-  InventoryItem item;
 
   cout << "Choose a item Number: ";
   cin >> itemIndex;
 
-  item = inventory[itemIndex];
+    InventoryItem& item = inventory[itemIndex];
 
   if(item.getDescription().empty()) {
     cout << "Error: item is not in use. Select another item." << endl;
@@ -166,6 +170,120 @@ void removeParts(InventoryItem inventory[]) {
 
 }
 
-void inputFromFile(int &itemCount, InventoryItem inventory[]) {
+int splitLineToArray(string inputText, string inputDelim,
+	string outputFieldArray[], int maxFields)
+{
+	//	Local Variables:
+	int nextFieldPosition = 0;		//	Start position of data field.
+	int foundPosition;				//  Position where delimiter was found,
+	int fieldCount = 0;		        //  Number of data fields found so far,
+	bool endOfLine = false;			//	Flag -- end of input line detected,
+	string dataField;				//  Copy of the data field.
 
+	while (!endOfLine)
+	{
+		foundPosition = inputText.find(inputDelim, nextFieldPosition);
+		if (foundPosition == string::npos)
+		{
+			// (Here if the string 'find' function reached the end of the input string.)
+			endOfLine = true;
+			foundPosition = inputText.length();
+
+			if (foundPosition > nextFieldPosition)
+			{
+				dataField = inputText.substr(nextFieldPosition,
+					foundPosition - nextFieldPosition);
+				fieldCount++;
+			}
+		}
+		else
+		{
+			// (Here if 'inputDelim' was found in the input string.)
+			// Copy the data field contents from the input string to the
+			// 'dataField' local variable.
+			dataField = inputText.substr(nextFieldPosition, foundPosition - nextFieldPosition);
+			fieldCount++;
+
+			// Adjust 'nextFieldPosition' as preparation for finding NEXT delimiter.
+			nextFieldPosition = foundPosition + 1;
+		}
+
+		if (fieldCount > 0)
+		{
+			if (fieldCount > maxFields)
+			{
+				cout << "ERROR at line __LINE__: too many input fields, fieldCount="
+					<< fieldCount << endl;
+			}
+			else
+			{
+				outputFieldArray[fieldCount - 1] = dataField;
+			}
+		}
+	} // (end while)
+
+
+	return fieldCount;
+} // (end function splitLineToArray )
+
+void inputFromFile(int &itemCount, InventoryItem inventory[]) {
+  string fileName;
+  ifstream inputFile;
+
+  cout << "Enter name of input file: ";
+  cin >> fileName;
+  int itemsAdded = 0;
+  string data;
+
+  inputFile.open(fileName);
+
+  if(!inputFile.is_open()) {
+    cout << "Error: File cannot be open or does not exist" << endl;
+    return;
+  }
+
+  while(getline(inputFile, data)) {
+    string productDetails[4];
+    splitLineToArray(data, "|", productDetails, 4);
+    if(itemCount < 100) {
+      inventory[itemCount] = InventoryItem(productDetails[1], stod(productDetails[2]), stoi(productDetails[3]));
+      itemCount++;
+      itemsAdded++;
+    } else {
+      cout << "Error: Cannot add more items, inventory is full" << endl;
+      break;
+    }
+  }
+
+
+  cout << itemsAdded << " records loaded to array." << endl;
+  inputFile.close();
+
+}
+
+void outputFromFile(int &itemCount, InventoryItem inventory[]) {
+  string fileName;
+  int itemsAdded;
+  ofstream outputFile;
+  
+  cout << "Enter name of output file: ";
+  cin >> fileName;
+
+  outputFile.open(fileName);
+
+  if(!outputFile.is_open()) {
+    cout << "Error: File cannot be open or does not exist" << endl;
+    return;
+  }
+
+  for(int i = 0; i < itemCount; i++) {
+    outputFile << i << " | " 
+    << inventory[i].getDescription() << " | " 
+    << fixed << setprecision(2) << inventory[i].getCost() << " | " 
+    << inventory[i].getUnits() << endl;
+    itemsAdded++;
+  }
+
+  cout << itemsAdded << " records written to file." << endl;
+  outputFile.close();
 }
