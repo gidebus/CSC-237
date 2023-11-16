fe5dd1e67ee7adedd6beac9ae0ab9c4f32f724f3
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-11-15 19:21:19 -0500

    Rename pdf file and remove unused cpp

---
+++ project2/InventoryItem.cpp
@@ -0,0 +1,50 @@
+
+#include "InventoryItem.h"
+#include <string>
+
+using namespace std;
+
+// // Constructors
+// InventoryItem::InventoryItem() {
+//   description = "";
+//   cost = 0.0;
+//   units = 0;
+// }
+
+// InventoryItem::InventoryItem(string description) {
+//   description = description;
+//   cost = 0.0;
+//   units = 0;
+// }
+
+// InventoryItem::InventoryItem(string description, double cost, int units) {
+//   description = description;
+//   cost = cost;
+//   units = units;
+// }
+
+// // Mutator methods
+// void InventoryItem::setDescription(string description) {
+//   description = description;
+// }
+
+// void InventoryItem::setCost(double cost) {
+//   cost = cost;
+// }
+
+// void InventoryItem::setUnits(int units) {
+//   units = units;
+// }
+
+// // Accessor functions
+// string InventoryItem::getDescription() const {
+//   return description;
+// }
+
+// double InventoryItem::getCost() const {
+//   return cost;
+// }
+
+// int InventoryItem::getUnits() const {
+//   return units;
+// }
