ecf8658c232358bcc8c350dbf1c984c73b2be8be
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-11-19 18:29:29 -0500

    Add header file for IntList class

---
+++ lab18a/IntList.h
@@ -0,0 +1,37 @@
+// Specification file for the IntList class
+#ifndef NUMBERLIST_H
+#define NUMBERLIST_H
+
+#include <string>
+
+class IntList
+{
+  private:
+    // Declare a structure for the list
+    struct ListNode
+    {
+        int value;           // The value in this node
+        struct ListNode *next;  // To point to the next node
+        ListNode(int value) : value(value), next(nullptr) {};
+    }; 
+
+    ListNode *head;            // List head pointer
+
+  public:
+    // Constructor
+    IntList() : head(nullptr) {};
+        
+    // Destructor
+    ~IntList();
+        
+    // Linked list operations
+    void appendNode(int); // Adds a node to the back
+    void insertNode(int); // Adds a node keeping the order
+    void deleteNode(int); // Deletes node with the matching value
+    void maxValue(); // Displays the highest data value in the list
+    void print(); // Outputs the values for all the node to the terminal
+    int length(); // Outputs how many nodes are in the list
+    int total(); // Returns the sum of all the values in the list
+};
+
+#endif
