09e2559a86ec1a01450d05e5b5e02466999fcc38
gidebus <40724004+GiDeBus@users.noreply.github.com>
2023-12-08 19:32:54 -0500

    Add f, a, and p methods

--- project3/Project3.cpp
+++ project3/Project3.cpp
@@ -36,7 +36,7 @@ void createCargoPlane(vector<Airplane*>& fleet) {
 
   CargoPlane* newCargoPlane = new CargoPlane(manufacturer, model, year, flightHours, maxCargoWeight);
   fleet.push_back(newCargoPlane);
-  cout << "\nNew Airplane: " << newCargoPlane->getDescription();
+  cout << "\nNew Airplane: \n" << newCargoPlane->getDescription();
 }
 
 void createPassengerPlane(vector<Airplane*>& fleet) {
@@ -60,7 +60,41 @@ void createPassengerPlane(vector<Airplane*>& fleet) {
 
   PassengerPlane* newPassengerPlane = new PassengerPlane(manufacturer, model, year, flightHours, maxNumberOfPassengers);
   fleet.push_back(newPassengerPlane);
-  cout << "\nNew Airplane: " << newPassengerPlane->getDescription();
+  cout << "\nNew Airplane: \n" << newPassengerPlane->getDescription();
+}
+
+void flyAirplane(vector<Airplane*>& fleet) {
+  int airplaneID, flightDuration;
+  Airplane* foundAirplane = nullptr;
+  
+  cout << "\nWhich airplane do you want to fly?: ";
+  cin >> airplaneID;
+
+  for(size_t i = 0; i < fleet.size(); ++i) {
+    if(fleet[i]->getAirplane_ID() == airplaneID) {
+      foundAirplane = fleet[i];
+      break;
+    }
+  }
+
+  if(foundAirplane != nullptr) {
+    cout << "\nLength of flight (hours)?: ";
+    cin >> flightDuration;
+
+    cout << "\nChoosing airplane: \n" << foundAirplane->getDescription();
+    foundAirplane->setFlightHours(foundAirplane->getFlightHours() + flightDuration);
+    cout << "\nFlight complete: \n" << foundAirplane->getDescription();
+
+  } else {
+    cout << "\nAirplane " << airplaneID << " not found. " << endl;
+  }
+}
+
+void printFleet(vector<Airplane*>& fleet) {
+  cout << "\nCurrent fleet contains " << fleet.size() << " airplane(s):" << endl;
+  for(size_t i = 0; i < fleet.size(); i++) {
+    cout << endl << fleet[i]->getDescription() << endl;
+  }
 }
 
 int main() {
@@ -89,11 +123,11 @@ int main() {
         createPassengerPlane(fleet);
       };
 
-      cout << "\naddNewAirplane: Size of fleet = " << fleet.size() << endl;
+      cout << "\naddNewAirplane: \nSize of fleet = " << fleet.size() << endl;
     };
     
     if(userInput == 'f') {
-      cout << "You chose F" << endl;
+      flyAirplane(fleet);
     };
 
     if(userInput == 'h') {
@@ -105,7 +139,7 @@ int main() {
     };
 
     if(userInput == 'p') {
-      cout << "You chose P" << endl;
+      printFleet(fleet);
     };
   
     if(userInput == 'r') {
